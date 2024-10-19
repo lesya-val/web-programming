179b916ad64451ff0264ce3f0d7deaa635de4af2
lesya-val <you@example.com>
2024-10-19 18:39:57 +0600

    Добавить проверку на логин

--- php/register.php
+++ php/register.php
@@ -1,15 +1,25 @@
 <?php
 require_once('db.php');
+
 $login = $_POST['login'];
 $password = $_POST['password'];
 
-// Хеширование пароля
-$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+// Проверка, существует ли уже пользователь с таким логином
+$sql_check = "SELECT * FROM users WHERE login = '$login'";
+$result = $conn->query($sql_check);
 
-// Добавление пользователя в БД
-$sql = "INSERT INTO users (login, password, role) VALUES ('$login', '$hashed_password', 'user')";
-if ($conn->query($sql) === TRUE) {
-	header("Location: ../index.php");
+if ($result->num_rows > 0) {
+	// Если пользователь с таким логином уже существует, выводим сообщение
+	echo "Пользователь с таким логином уже существует. Выберите другой логин.";
 } else {
-	echo "Ошибка: " . $conn->error;
+	// Хеширование пароля
+	$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+	// Добавление пользователя в БД
+	$sql = "INSERT INTO users (login, password, role) VALUES ('$login', '$hashed_password', 'user')";
+	if ($conn->query($sql) === TRUE) {
+			header("Location: ../index.php");  // Перенаправление после успешной регистрации
+	} else {
+			echo "Ошибка: " . $conn->error;
+	}
 }
